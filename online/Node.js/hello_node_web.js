9931fa8ef15b7a2d96127838eadea46b5600a1c5
Nathan Krasney <natankrasney@gmail.com>
2019-08-11 12:50:25 +0300

    hello node web

---
+++ online/Node.js/hello_node_web.js
@@ -0,0 +1,9 @@
+var http = require('http');
+console.log('open the browser at localhost:\8080')
+
+const requestHandler= (request, response) => {
+   response.writeHead(200, {'Content-Type': 'text/html'});
+   response.write('<h1 style="color:red;">Hello Node.js!</h1>');
+   response.end();
+}
+http.createServer(requestHandler).listen(8080);
